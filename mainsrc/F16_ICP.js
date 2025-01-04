55f972a887a23dd15a15f742820823b992be7428
DaveC3 <dcutliff@suddenlink.net>
2025-01-04 10:10:29 -0600

    Add files via upload

--- mainsrc/F16_ICP.js
+++ mainsrc/F16_ICP.js
@@ -7,9 +7,9 @@ module.exports.init = function (_WWTHIDJSAPI, XpData) {
     g_XpData = XpData
     WWTHIDJSAPI = _WWTHIDJSAPI;
 	WWTHIDJSAPI.ICPDrawStart();
-	WWTHIDJSAPI.ICPDraw(0, 0,  "COM1" + g_XpData[0], false, "DCS");
+	WWTHIDJSAPI.ICPDraw(0, 0,  "COM1 " + g_XpData[0], false, "DCS");
 	WWTHIDJSAPI.ICPDraw(100, 0,  "STBY " + g_XpData[1], true, "DCS");	
-	WWTHIDJSAPI.ICPDraw(0, 15, "NAV1" + g_XpData[2], false, "DCS");
+	WWTHIDJSAPI.ICPDraw(0, 15, "NAV1 " + g_XpData[2], false, "DCS");
 	WWTHIDJSAPI.ICPDraw(100, 15, "STBY " + g_XpData[3], true, "DCS");	
 	WWTHIDJSAPI.ICPDraw(0, 30, "HDG " + g_XpData[4], false, "DCS");
 	WWTHIDJSAPI.ICPDraw(100, 30, "ALT " + g_XpData[5], false, "DCS");	
